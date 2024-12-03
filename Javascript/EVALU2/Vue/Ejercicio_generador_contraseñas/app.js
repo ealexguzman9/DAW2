71f946a38bd571b45eb0d71ed4a6d2b580339ed0
ealexguzman9 <eguzmancruz47@gmail.com>
2024-12-03 18:33:11 +0100

    Generador de contras

--- Javascript/EVALU2/Vue/Ejercicio_generador_contraseñas/app.js
+++ Javascript/EVALU2/Vue/Ejercicio_generador_contraseñas/app.js
@@ -3,32 +3,70 @@ const { createApp } = Vue;
 const App = {
   data() {
     return {
-      letra:"",
-      minusculas:"abcdefghijklmnopqrstuvwxyz"
+      caja: "",
+      longitud: 8,
+      minusculas: "abcdefghijklmnopqrstuvwxyz", 
+      mayusculas: "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 
+      digitos: "0123456789", 
+      simbolos: "!@#$%^&*()_-+=<>?/[]{}",
+      caracteresPermitidos: "", 
+      incluirMinusculas: false, 
+      incluirMayusculas: false, 
+      incluirDigitos: false, 
+      incluirSimbolos: false, 
     };
   },
   methods: {
-    generar_contraseña(){
-    this.letra = minusculas.charAt(Math.floor(Math.random() * minusculas.length))
+    generar_contraseña() {
+      let caracteresDisponibles = "";
+      if (this.incluirMinusculas) caracteresDisponibles += this.minusculas;
+      if (this.incluirMayusculas) caracteresDisponibles += this.mayusculas;
+      if (this.incluirDigitos) caracteresDisponibles += this.digitos;
+      if (this.incluirSimbolos) caracteresDisponibles += this.simbolos;
+
+      if (caracteresDisponibles.length === 0) {
+        this.caja = "Por favor, selecciona al menos una opción";
+        return;
+      }
+      let password = "";
+      for (let i = 0; i < this.longitud; i++) {
+        const randomChar = caracteresDisponibles.charAt(Math.floor(Math.random() * caracteresDisponibles.length));
+        password += randomChar;
+      }
+      this.caja = password;
+    },
+    copiarAlPortapapeles() {
+      navigator.clipboard.writeText(this.caja)
+        .then(() => {
+          alert("Contraseña copiada al portapapeles");
+        })
+        .catch(err => {
+          alert("Error al copiar la contraseña: " + err);
+        });
     }
   },
   template: `
-    <input 
-    v-model="caja"
-    type="textbox"
-    placeholder=""
-    />
-    <button type="submit">Copiar</button><br>
-    <input type="range" min="1" max="20" v-model="longitud">
+    <div>
+      <input 
+        v-model="caja"
+        type="text"
+        placeholder="Contraseña generada"
+        readonly
+      />
+      <button type="button" @click="copiarAlPortapapeles">Copiar</button><br>
+      
+      <label>Longitud: {{ longitud }}</label>
+      <input type="range" min="1" max="20" v-model="longitud" /><br>
 
-    <br>
-    <label><input type="checkbox" v-model="minusculas">Minúsculas</label>
-    <label><input type="checkbox" v-model="minusculas">Mayúsculas</label>
-    <label><input type="checkbox" v-model="digitos">Dígitos</label>
-    <label><input type="checkbox" v-model="simbolos">Símbolos</label><br>
-    <button type="submit" @click="generar_contraseña">Generar</button>
+      <!-- Checkboxes para elegir los tipos de caracteres -->
+      <label><input type="checkbox" v-model="incluirMinusculas"> Minúsculas</label>
+      <label><input type="checkbox" v-model="incluirMayusculas"> Mayúsculas</label>
+      <label><input type="checkbox" v-model="incluirDigitos"> Dígitos</label>
+      <label><input type="checkbox" v-model="incluirSimbolos"> Símbolos</label><br>
+
+      <button type="button" @click="generar_contraseña">Generar Contraseña</button>
+    </div>
   `,
 };
-/*Navigator.clipboard.writeText("aqui ira la variable a copiar")*/
-// Crear la aplicación y montarla en el elemento con id="app"
+
 createApp(App).mount('#app');
