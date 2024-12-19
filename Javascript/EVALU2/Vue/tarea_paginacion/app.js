bd0e13fde6cc0c5fa56a0a5504e93dfad85c94c7
ealexguzman9 <er.guzman@aulanz.net>
2024-12-19 13:56:31 +0100

    Cambios pagina

--- Javascript/EVALU2/Vue/tarea_paginacion/app.js
+++ Javascript/EVALU2/Vue/tarea_paginacion/app.js
@@ -8,9 +8,9 @@ createApp({
       <li v-for="comida in paginatedfoods" :key="comida.id">
         <img :src="comida.imagen" :alt="comida.comida" width="100" height="100" />
         <div>
-          <strong>{{ comida.comida }}</strong> ({{ comida.origenes.toLocaleString() }} Origenes)
+          <strong>{{ comida.comida }}</strong> (Origenes: {{ comida.origenes.toLocaleString() }})
           <br>
-          {{ comida.pais }}, {{ comida.continente }}
+          País: {{ comida.pais }}, Continente: {{ comida.continente }}
         </div>
       </li>
     </ul>
@@ -34,7 +34,7 @@ createApp({
         { id: 7, comida: "Pancakes", origenes: "Antigua Grecia", pais: "Grecia", continente: "Europa", imagen: "img/pancakes.webp" },
         { id: 8, comida: "Pasta parmesana", origenes: "Italia", pais: "Italia", continente: "Europa", imagen: "img/pasta_parmesana.webp" },
         { id: 9, comida: "Pastel de chocolate", origenes: "America Central", pais: "Honduras, Costa Rica y Nicaragua", continente: "America", imagen: "img/pastel_chocolate.webp" }, 
-        { id: 10, comida: "pizza", origenes: "Italia", pais: "Italia", continente: "Europa", imagen: "img/pizza.webp" },
+        { id: 10, comida: "Pizza", origenes: "Italia", pais: "Italia", continente: "Europa", imagen: "img/pizza.webp" },
         { id: 11, comida: "Ravioles", origenes: "Italia", pais: "Italia", continente: "Europa", imagen: "img/ravioles.webp" },
         { id: 12, comida: "Salmon Teriyaki", origenes: "Japón", pais: "Japon", continente: "Asia", imagen: "img/salmon_teriyaki.webp" },
         { id: 13, comida: "Sushi", origenes: "Japón", pais: "Japón", continente: "Asia", imagen: "img/sushi.webp" },
